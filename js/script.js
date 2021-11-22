98a4f2c323a7ff6015e1002ef7cbb014cb4f2457
unknown <nguyenhuuthinh1208@gmail.com>
2021-11-22 17:04:11 +0700

    upload source code

---
+++ js/script.js
@@ -0,0 +1,332 @@
+// const countries = [
+//     'Afghanistan',
+//     'Albania',
+//     'Algeria',
+//     'Andorra',
+//     'Angola',
+//     'Antigua and Barbuda',
+//     'Argentina',
+//     'Armenia',
+//     'Australia',
+//     'Austria',
+//     'Azerbaijan',
+//     'Bahamas',
+//     'Bahrain',
+//     'Bangladesh',
+//     'Barbados',
+//     'Belarus',
+//     'Belgium',
+//     'Belize',
+//     'Benin',
+//     'Bhutan',
+//     'Bolivia',
+//     'Bosnia and Herzegovina',
+//     'Botswana',
+//     'Brazil',
+//     'Brunei',
+//     'Bulgaria',
+//     'Burkina Faso',
+//     'Burundi',
+//     'Cambodia',
+//     'Cameroon',
+//     'Canada',
+//     'Cape Verde',
+//     'Central African Republic',
+//     'Chad',
+//     'Chile',
+//     'China',
+//     'Colombi',
+//     'Comoros',
+//     'Congo (Brazzaville)',
+//     'Congo',
+//     'Costa Rica',
+//     "Cote d'Ivoire",
+//     'Croatia',
+//     'Cuba',
+//     'Cyprus',
+//     'Czech Republic',
+//     'Denmark',
+//     'Djibouti',
+//     'Dominica',
+//     'Dominican Republic',
+//     'East Timor (Timor Timur)',
+//     'Ecuador',
+//     'Egypt',
+//     'El Salvador',
+//     'Equatorial Guinea',
+//     'Eritrea',
+//     'Estonia',
+//     'Ethiopia',
+//     'Fiji',
+//     'Finland',
+//     'France',
+//     'Gabon',
+//     'Gambia, The',
+//     'Georgia',
+//     'Germany',
+//     'Ghana',
+//     'Greece',
+//     'Grenada',
+//     'Guatemala',
+//     'Guinea',
+//     'Guinea-Bissau',
+//     'Guyana',
+//     'Haiti',
+//     'Honduras',
+//     'Hungary',
+//     'Iceland',
+//     'India',
+//     'Indonesia',
+//     'Iran',
+//     'Iraq',
+//     'Ireland',
+//     'Israel',
+//     'Italy',
+//     'Jamaica',
+//     'Japan',
+//     'Jordan',
+//     'Kazakhstan',
+//     'Kenya',
+//     'Kiribati',
+//     'Korea, North',
+//     'Korea, South',
+//     'Kuwait',
+//     'Kyrgyzstan',
+//     'Laos',
+//     'Latvia',
+//     'Lebanon',
+//     'Lesotho',
+//     'Liberia',
+//     'Libya',
+//     'Liechtenstein',
+//     'Lithuania',
+//     'Luxembourg',
+//     'Macedonia',
+//     'Madagascar',
+//     'Malawi',
+//     'Malaysia',
+//     'Maldives',
+//     'Mali',
+//     'Malta',
+//     'Marshall Islands',
+//     'Mauritania',
+//     'Mauritius',
+//     'Mexico',
+//     'Micronesia',
+//     'Moldova',
+//     'Monaco',
+//     'Mongolia',
+//     'Morocco',
+//     'Mozambique',
+//     'Myanmar',
+//     'Namibia',
+//     'Nauru',
+//     'Nepal',
+//     'Netherlands',
+//     'New Zealand',
+//     'Nicaragua',
+//     'Niger',
+//     'Nigeria',
+//     'Norway',
+//     'Oman',
+//     'Pakistan',
+//     'Palau',
+//     'Panama',
+//     'Papua New Guinea',
+//     'Paraguay',
+//     'Peru',
+//     'Philippines',
+//     'Poland',
+//     'Portugal',
+//     'Qatar',
+//     'Romania',
+//     'Russia',
+//     'Rwanda',
+//     'Saint Kitts and Nevis',
+//     'Saint Lucia',
+//     'Saint Vincent',
+//     'Samoa',
+//     'San Marino',
+//     'Sao Tome and Principe',
+//     'Saudi Arabia',
+//     'Senegal',
+//     'Serbia and Montenegro',
+//     'Seychelles',
+//     'Sierra Leone',
+//     'Singapore',
+//     'Slovakia',
+//     'Slovenia',
+//     'Solomon Islands',
+//     'Somalia',
+//     'South Africa',
+//     'Spain',
+//     'Sri Lanka',
+//     'Sudan',
+//     'Suriname',
+//     'Swaziland',
+//     'Sweden',
+//     'Switzerland',
+//     'Syria',
+//     'Taiwan',
+//     'Tajikistan',
+//     'Tanzania',
+//     'Thailand',
+//     'Togo',
+//     'Tonga',
+//     'Trinidad and Tobago',
+//     'Tunisia',
+//     'Turkey',
+//     'Turkmenistan',
+//     'Tuvalu',
+//     'Uganda',
+//     'Ukraine',
+//     'United Arab Emirates',
+//     'United Kingdom',
+//     'United States',
+//     'Uruguay',
+//     'Uzbekistan',
+//     'Vanuatu',
+//     'Vatican City',
+//     'Venezuela',
+//     'Vietnam',
+//     'Yemen',
+//     'Zambia',
+//     'Zimbabwe'
+// ]
+
+
+// //   for(let country in countries){
+// //       console.log(country)
+// //   }
+
+// /*
+//   foreach
+//   map
+//   filter
+//   find
+
+// */
+
+// // let square = (function (a) {
+// //     return a * a
+// // })(2)
+// // console.log(square)
+
+// // console.log(Math.max(1, 2, 3, 4, 5))
+
+// // function findMax() {
+// //     let max = arguments[0]
+// //     for (let item of arguments) {
+// //         if (item > max) {
+// //             max = item
+// //         }
+// //     }
+// //     return max
+// // }
+
+// // function showDateTime() {
+// //     let now = new Date()
+// //     let year = now.getFullYear()
+// //     let month = now.getMonth() + 1
+// //     let day = now.getDate()
+
+// //     let hour = now.getHours()
+// //     let minutes = now.getMinutes()
+
+// //     return `${day}/${month}/${year} ${hour}:${minutes}`
+// // }
+
+// // console.log(showDateTime())
+
+// // function arrayOfHexaColors() {
+// //     const hexastr = "123456789abcdef"
+// //     let hexaColor = '#'
+// //     for (let i = 0; i < 6; i++) {
+// //         let indexItem = Math.floor(Math.random()*(hexaColor.length))
+// //         hexaColor+= hexastr[indexItem]
+// //     }
+// //     return hexaColor
+// // }
+
+// // console.log(arrayOfHexaColors())
+
+// // // var str1 
+// // let str1
+// // console.log(str1)
+// // str1 = 'thinh'
+
+
+
+// const users = [
+//      {
+//       email: 'alex@alex.com',
+//       skills: ['HTML', 'CSS', 'JavaScript'],
+//       age: 20,
+//       isLoggedIn: false,
+//       points: 30
+//     },
+//      {
+//       email: 'asab@asab.com',
+//       skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+//       age: 25,
+//       isLoggedIn: false,
+//       points: 50
+//     },
+//      {
+//       email: 'daniel@daniel.com',
+//       skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+//       age: 30,
+//       isLoggedIn: true,
+//       points: 50
+//     },
+//     {
+//       email: 'daniel@alex.com',
+//       skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+//       age: 20,
+//       isLoggedIn: false,
+//       points: 40
+//     },
+//     {
+//       email: 'john@john.com',
+//       skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+//       age: 20,
+//       isLoggedIn: true,
+//       points: 50
+//     },
+//     {
+//       email: 'thomas@thomas.com',
+//       skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+//       age: 20,
+//       isLoggedIn: false,
+//       points: 40
+//     },
+//     {
+//       email: 'paul@paul.com',
+//       skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+//       age: 20,
+//       isLoggedIn: false,
+//       points: 40
+//     }
+//   ]
+
+
+
+//   // let countLoggedUsers = 0;
+
+//   // for(let user in users){
+//   //     if(users[user].isLoggedIn == true) countLoggedUsers++
+//   // }
+
+//   let arrNew = users.map(function(user) {
+//     return user.skills;
+//   })
+
+//   let arrnew = []
+//   for(let i = 0;i<arrNew.length;i++){
+//     arrnew = arrnew.concat(arrNew[i])
+//   }
+
+//   console.log(arrnew)
+
+  
+  
